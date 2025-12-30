842897f267b9c877e93a890628f75e4f4cd09cf8
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-12-30 20:56:42 +0300

    Program

--- SmartHomeSystem/Program.cs
+++ SmartHomeSystem/Program.cs
@@ -5,17 +5,13 @@ namespace SmartHomeSystem
 {
     internal static class Program
     {
-        /// <summary>
-        /// Uygulamanın ana giriş noktası.
-        /// </summary>
+      
         [STAThread]
         static void Main()
         {
-            // Yüksek DPI ayarları ve varsayılan yazı tipi gibi uygulama yapılandırmalarını başlatır
             ApplicationConfiguration.Initialize();
 
-            // Ana form olan Form1'i belleğe yükler ve çalıştırır
-            // Form1 açıldığında Klima ve Perde nesneleri birbirinden bağımsız olarak oluşturulacaktır.
+           
             Application.Run(new Form1());
         }
     }
